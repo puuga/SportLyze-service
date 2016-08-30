a4ba7b79b92491f7f1706317e0fdd42a404b45ad
Siwawes Wongcharoen <siwaoh@gmail.com>
2016-08-30 20:51:17 +0700

    add user service

---
+++ service/user_info.php
@@ -0,0 +1,36 @@
+<?php
+include "include/db_connect_oo.php";
+
+include "model/user.php";
+?>
+<?php
+$conn = connect_db($db_server, $db_username, $db_password, $db_dbname);
+
+// read get data
+// firebase_id
+if( isset($_GET["firebase_id"]) ) {
+  $firebase_id = $_GET["firebase_id"];
+  $user = getUserWithFirebase($conn, $firebase_id);
+  if ( !is_null($user) ) {
+    header('Content-Type: application/json');
+    echo json_encode($user);
+    $conn->close();
+    exit();
+}
+
+// id
+if( isset($_GET["id"]) ) {
+  $id = $_GET["id"];
+  $user = getUserWithId($conn, $id);
+  if ( !is_null($user) ) {
+    header('Content-Type: application/json');
+    echo json_encode($user);
+    $conn->close();
+    exit();
+}
+
+http_response_code(204);
+$conn->close();
+exit();
+
+?>
